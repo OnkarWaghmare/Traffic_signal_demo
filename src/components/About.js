f0e0782e28cb1486133125bc0a8cf528c00568c4
Onkar Waghmare <76032058+OnkarWaghmare@users.noreply.github.com>
2025-02-13 06:32:05 +0000

    updated signal logic for variable time slots

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,21 @@
 import { useSelector } from "react-redux";
 import { useState, useEffect } from "react";
 const About = () => {
-  const [clr, setClr] = useState("green");
+  const [clr, setClr] = useState("red");
   const storeName = useSelector((store) => store.testSlice.data);
-  useState(() => {
-    const interval = setInterval(() => {
-      console.log("called", setClr);
-      setClr((prevState) => updateSignal(prevState));
-    }, 3000);
-    return () => clearInterval(interval);
-  }, []);
+  useEffect(() => {
+    console.log("called", clr);
+    if (clr == "red") {
+      setTimeout(() => setClr((prevState) => updateSignal(prevState)), 4000);
+    } else if (clr == "yellow") {
+      setTimeout(() => setClr((prevState) => updateSignal(prevState)), 500);
+    } else if (clr == "green") {
+      setTimeout(() => setClr((prevState) => updateSignal(prevState)), 3000);
+    }
+  }, [clr]);
+
   const updateSignal = (prevState) => {
+    console.log(prevState);
     if (prevState == "red") {
       return "yellow";
     } else if (prevState == "yellow") {
